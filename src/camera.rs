b33a0600c8558b7dfb95d67807ef95c4d58da052
Carlo Supina <cdsupina@gmail.com>
2022-03-27 20:31:58 -0500

    remove .vscode and update gitignore

--- src/camera.rs
+++ src/camera.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
 use bevy_parallax::ParallaxCameraComponent;
 
+// UI camera tag component
 #[derive(Component)]
 pub struct UICameraComponent;
 
+// Initialize UI camera and 2D camera with parallax
 pub fn add_camera(mut commands: Commands) {
-    info!("Spawning cameras");
     commands
         .spawn_bundle(UiCameraBundle::default())
         .insert(UICameraComponent);
--- src/states.rs
+++ src/states.rs
@@ -2,12 +2,12 @@ use bevy::prelude::*;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Component)]
 pub enum GameStates {
-    Main,
-    ConsoleLoading,
-    Console,
-    PauseMenu,
-    TabMenuLoading,
-    TabMenu,
-    GameOver,
-    MainMenu,
+    Main,           // main runner game
+    ConsoleLoading, // loading the console
+    Console,        // console/terminal
+    PauseMenu,      // in-game pause menu
+    TabMenuLoading, // loading the journal
+    TabMenu,        // journal/stats
+    GameOver,       // game over menu
+    MainMenu,       // main menu
 }
